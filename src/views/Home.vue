aa206d656f8bc0b92f028b31238e35ace7d714c9
lucida <zheyizhifeng@126.com>
2022-08-22 16:27:27 +0800

    feat: update vue2-js template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="wrap">
-    <div class="text">Hello Vue2 + Vite</div>
-    <img class="vue-logo" src="../assets/images/Vue.png" alt="" />
+  <div>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="@images/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
   </div>
 </template>
 
-<script></script>
+<script>
+  import HelloWorld from "./components/HelloWorld.vue";
+  import "./style.css";
+  export default {
+    components: {
+      HelloWorld,
+    },
+  };
+</script>
 
-<style lang="scss" scoped>
-  .wrap {
-    @include flex(column);
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
 
-    .vite-logo {
-      max-height: 200px;
-    }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
   }
 </style>
