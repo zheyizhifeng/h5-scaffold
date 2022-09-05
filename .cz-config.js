60ade0f81d615aa2c9e927ab6f83381c729e63ea
lucida <zheyizhifeng@126.com>
2022-09-05 15:49:00 +0800

    :sparkles: feat(custom): update commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -1,56 +1,65 @@
 module.exports = {
   types: [
-    { value: ":sparkles: feat", name: "✨ feat:\tAdding a new feature" },
-    { value: ":bug: fix", name: "🐛 fix:\tFixing a bug" },
-    { value: ":memo: docs", name: "📝 docs:\tAdd or update documentation" },
+    {
+      value: ":sparkles: feat",
+      name: "✨ feat:     新功能",
+    },
+    {
+      value: ":bug: fix",
+      name: "🐛 fix:      修复bug",
+    },
+    {
+      value: ":tada: init",
+      name: "🎉 init:     初始化",
+    },
+    {
+      value: ":pencil2: docs",
+      name: "✏️  docs:     文档变更",
+    },
     {
       value: ":lipstick: style",
-      name: "💄 style:\tAdd or update styles, ui or ux",
+      name: "💄 style:    代码的样式美化",
     },
     {
       value: ":recycle: refactor",
-      name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature",
+      name: "♻️  refactor: 重构",
     },
     {
       value: ":zap: perf",
-      name: "⚡️ perf:\tCode change that improves performance",
+      name: "⚡️ perf:     性能优化",
     },
     {
       value: ":white_check_mark: test",
-      name: "✅ test:\tAdding tests cases",
+      name: "✅ test:     测试",
     },
     {
-      value: ":truck: chore",
-      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
-    },
-    { value: ":rewind: revert", name: "⏪️ revert:\tRevert to a commit" },
-    { value: ":construction: wip", name: "🚧 wip:\tWork in progress" },
-    {
-      value: ":construction_worker: build",
-      name: "👷 build:\tAdd or update regards to build process",
+      value: ":rewind: revert",
+      name: "⏪️ revert:   回退",
     },
     {
-      value: ":green_heart: ci",
-      name: "💚 ci:\tAdd or update regards to build process",
+      value: ":package: build",
+      name: "📦️ build:    打包",
+    },
+    {
+      value: ":rocket: chore",
+      name: "🚀 chore:    构建/工程依赖/工具",
+    },
+    {
+      value: ":construction_worker: ci",
+      name: "👷 ci:       CI related changes",
     },
   ],
-
-  scopes: [
-    { name: "ui" },
-    { name: "android" },
-    { name: "ios" },
-    { name: "home" },
-    { name: "planner" },
-    { name: "settings" },
-  ],
-
-  scopeOverrides: {
-    fix: [{ name: "merge" }, { name: "style" }, { name: "test" }, { name: "hotfix" }],
+  messages: {
+    type: "请选择提交类型(必填)",
+    customScope: "请输入文件修改范围(可选)",
+    subject: "请简要描述提交(必填)",
+    body: "请输入详细描述(可选)",
+    breaking: "列出任何BREAKING CHANGES(可选)",
+    footer: "请输入要关闭的issue(可选)",
+    confirmCommit: "确定提交此说明吗？",
   },
-
   allowCustomScopes: true,
-  allowBreakingChanges: ["feat", "fix"],
-  // skip any questions you want
+  allowBreakingChanges: [":sparkles: feat", ":bug: fix"],
   skipQuestions: ["body"],
   subjectLimit: 100,
 };
